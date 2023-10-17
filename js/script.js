f5153f94aed129e8b878bd6da8b3537548de0c28
Hycarus <mirkomarasco08@gmail.com>
2023-10-17 15:12:58 +0200

    created function palindrome

--- js/script.js
+++ js/script.js
@@ -1,4 +1,36 @@
 const btn = document.querySelector('button');
-const inputEl = document.getElementById('data');
+
 const resultEl = document.querySelector('.alert');
+
+
+
+btn.addEventListener('click', function(){
+    const inputEl = document.getElementById('data').value;
+    palindrome(inputEl);
+})
+
+
+function palindrome(input){
+    let sliceArray = [];
+    let reverseArray = [];
+    console.log(input)
+    for(let i = 0; i < input.length; i++){
+        console.log(input[i]);
+        sliceArray.push(input[i]);
+    }
+    for(let i = input.length - 1; i >= 0; i--){
+        console.log(input[i]);
+        reverseArray.push(input[i]);
+    }
+    console.log(sliceArray);
+    console.log(reverseArray);
+    let word1 = sliceArray.join('');
+    let word2 = reverseArray.join('');
+    if(word1 === word2){
+        console.log('è palindromo');
+    } else{
+        console.log('non è palindromo');
+    }
+}
+
 
